249e0f965d85db57d421e719386a47df50a41809
aminamos <26092352+aminamos@users.noreply.github.com>
2019-06-22 12:57:43 -0700

    widget file updates

--- widgets.rb
+++ widgets.rb
@@ -1,12 +1,39 @@
 require 'pry'
 
+# receive user input for desired number of widgets
+# assume that negative numbers are accidental, switch to positive integer
 def ask(question)
     print question + " "
-    gets.chomp
+    response = gets.chomp.to_i
+    if response < 0
+        response = response * -1
+    end
+    response
 end
 
+# discount conditional
+def price(quantity)
+    if quantity >= 50 && quantity < 100
+        price_per_widget = 9
+    elsif quantity >= 100
+        price_per_widget = 8
+    else
+        price_per_widget = 10
+    end
+    quantity * price_per_widget
+end
 
+# apply good customer service
 puts "Welcome to the widget store!"
-answer = ask("How many widgets are you ordering?")
-p "You entered #{answer} widgets"
+
+# request a purchase
+answer = ask("Disclaimer: negative numbers will be transformed into positive numbers.\nHow many widgets are you ordering?")
+price_per_widget = 0
+total = price(answer)
+
+# verify number of widgets requested and display total
+p "You entered #{answer} widgets, making your total $#{total}."
+
+
+
 
